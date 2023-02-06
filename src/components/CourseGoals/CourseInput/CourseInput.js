a007213f7c2b719b899fad40dcf46d6fef3a80bb
Shree Devi Yadam <shree.yadam@gmail.com>
2023-02-06 14:35:57 -0800

    Set input state to valid or invalid

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,8 +6,12 @@ import "./CourseInput.css";
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
+
   const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
+    if(event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
   };
 
   const formSubmitHandler = (event) => {
